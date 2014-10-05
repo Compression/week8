a927c7941285cad9b3dc360b7441566140616644
Josh <rkb.josh@gmail.com>
2014-10-05 16:59:03 -0400

    car and manufacturer added

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,1 +1,8 @@
-class Car << ActiveRecord::Base
+class Car < ActiveRecord::Base
+
+  validates :manufacturer, presence: true
+  validates :color, presence: true
+  validates :year, presence: true, :value, :inclusion => {:in => [1920..2020]}
+  validates :mileage, presence: true
+  validates :description, length: { maximum: 255}
+end
